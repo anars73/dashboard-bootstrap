b2c9b774270281ca55eebdf0056ebf5d37e5b65b
anaruson <ahti.naruson@gmail.com>
2017-02-24 15:13:22 +0200

    Sync

--- dashboard-bootstrap/config/routes.rb
+++ dashboard-bootstrap/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :dashboard, only: [:index]
   root 'dashboard#index'
-  resources :companies
-  resources :employees
-
+  resources :companies do
+    resources :employees
+    end
 end
