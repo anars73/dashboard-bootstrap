575412ea44a4ebff8a9047956f44e3dbe89237aa
anaruson <ahti.naruson@gmail.com>
2017-03-02 00:15:34 +0200

    Sync

--- dashboard-bootstrap/app/models/company.rb
+++ dashboard-bootstrap/app/models/company.rb
@@ -1,4 +1,5 @@
 class Company < ApplicationRecord
   has_many :employees, dependent: :destroy
+  has_many :products
   validates :name, :description, presence: true
 end
--- dashboard-bootstrap/config/routes.rb
+++ dashboard-bootstrap/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   resources :products
   devise_for :admins
   resources :dashboard, only: [:index]
@@ -6,5 +7,7 @@ Rails.application.routes.draw do
   root 'dashboard#index'
   resources :companies do
     resources :employees
+    resources :categories
+    resources :products
   end
 end
